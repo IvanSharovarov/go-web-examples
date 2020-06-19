3cfc5b351294c8b0885791c24be967ac7ad1fe6f
Ivan Sharovarov <sharovarov31@gmail.com>
2020-06-19 19:23:22 +0600

    routing modify

--- routing mux/main.go
+++ routing mux/main.go
@@ -7,16 +7,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// ShowDocument returns document title and page number from request
+func ShowDocument(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	page := vars["page"]
+
+	fmt.Fprintf(w, "You've requested the document: %s on page %s\n", title, page)
+}
+
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/documents/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
-
-		fmt.Fprintf(w, "You've requested the document: %s on page %s\n", title, page)
-	})
+	r.HandleFunc("/documents/{title}/page/{page}", ShowDocument).Methods("GET")
 
 	http.ListenAndServe(":9000", r)
 }
